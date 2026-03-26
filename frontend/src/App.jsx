50601b2354cb49794f995d5647870bd8b5717b14
JEEVAN SRESANTH S <jeevan21406@gmail.com>
2026-03-26 22:08:42 +0530

    Initial react setup with jobform component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import JobForm from "./components/JobForm";
+function App(){
+  return (
+    <div>
+      <h1>Job Tracker</h1>
+      <p>My first React app!</p>
+      <JobForm />
+    </div> 
+  );
+}
+export default App; 
---
+++ frontend/src/components/JobForm.jsx
@@ -0,0 +1,8 @@
+function JobForm(){
+    return(
+        <div>
+            <h2> Add Job</h2>
+        </div>
+    );
+}
+export default JobForm;
